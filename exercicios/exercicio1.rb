03c526559018f87f1ab760c5163519c26d1483cf
Rafael <rafarsk78@gmail.com>
2025-09-02 11:45:34 -0300

    Resolução de exercicios

---
+++ exercicios/exercicio1.rb
@@ -0,0 +1,18 @@
+puts 'Digite um número: '
+num = gets.chomp.to_i
+
+puts '==============='
+puts "Tabuada do #{num}"
+puts '==============='
+
+puts "#{num*0}"
+puts "#{num*1}"
+puts "#{num*2}"
+puts "#{num*3}"
+puts "#{num*4}"
+puts "#{num*5}"
+puts "#{num*6}"
+puts "#{num*7}"
+puts "#{num*8}"
+puts "#{num*9}"
+puts "#{num*10}"
